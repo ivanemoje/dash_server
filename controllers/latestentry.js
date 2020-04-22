df6842f604c183cb197f8c0cbf9366527e507970
Ivan Emoje <Ivan.Emoje@wfp.org>
2020-04-22 12:56:51 +0300

    Revert

--- controllers/latestentry.js
+++ controllers/latestentry.js
@@ -2,7 +2,7 @@ const latestEntry = (req, res, pool) => {
       
   console.log('Latest entries requested by', req.connection.remoteAddress);
  
-  query_run2 = `SELECT 
+  query_run = `SELECT 
                 a.manifest_name,  a.settlement,  a.fdp, a.area, a.field, a.modality, a.begin_date, a.end_date, a.servedHH, a.servedPopn, 
                 a.plannedHH, a.plannedPopn, a.percent_HH, a.percent_Popn, a.hostname,
                 a.cycle, a.cycle_year,   a.create_date 
@@ -11,13 +11,7 @@ const latestEntry = (req, res, pool) => {
                 ON a.manifest_name = b.manifest_name 
                 AND a.create_date < b.create_date 
                 WHERE b.manifest_name IS NULL ORDER BY a.create_date desc`; 
-  query_run = `SELECT 
-                a.manifest_name
-                FROM gdt_ddr a 
-                LEFT OUTER JOIN gdt_ddr b 
-                ON a.manifest_name = b.manifest_name 
-                AND a.create_date < b.create_date 
-                WHERE b.manifest_name IS NULL ORDER BY a.create_date desc`; 
+
   pool.query(query_run, function (error, results, fields) {
     if (error) throw error;
     if (results.length) {
