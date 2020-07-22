4f655c2c96bb5f7c4c8c39eaf1733e5121a592a9
Ivan Emoje <Ivan.Emoje@wfp.org>
2020-07-22 12:05:25 +0300

    Cleaned irrelevant code

--- controllers/all.js
+++ controllers/all.js
@@ -22,35 +22,5 @@ const allQueries = (req, res, pool) => {
     };
 
 
-  //   pool.query(query_run, function (error, results) {
-  //     if (error) throw error;
-  //     let dateSt;
-  //     if (results.length) {
-  //       Object.keys(results).forEach(function(key){
-  //         var row = results[key];
-  //         const manifest_name = row.manifest_name;
-  //         const create_date = row.create_date.toLocaleDateString("en-US", {year: 'numeric',month: 'numeric',day: 'numeric',hour: 'numeric',minute: 'numeric'});
-  //         // console.log(manifest_name,create_date);
-  //         dateSt = (manifest_name + create_date);
-  //         console.log(dateSt);
-
-  //       // res.json({create_date:create_date});
-        
-  //       // res.json(dateSt);
-  //       })
-  //     // res.json(dateSt);
-  //     res.json(results);
-
-  //     }
-  //     else {
-  //       res.status(404).json('Not found');
-  //       }      
-  //   })
-  // }
-  
-  // module.exports = {
-  //   allQueries 
-  // }
-
 
   
